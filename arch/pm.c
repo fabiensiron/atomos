b00fb30195269a1035ec792c10fcd0dd10ec0f1f
saruta <fabiensiron@orange.fr>
2015-03-23 23:23:05 +0100

    pm done but bug code selector

--- arch/pm.c
+++ arch/pm.c
@@ -1,28 +1,53 @@
 #include "pm.h"
 
+static void load_segments () {
+  write_text_vga ("done\n");
+  __asm__ volatile (
+    "ljmp %0, $1f \n\
+    1: \n\
+    movw %1, %%ax \n\
+    movw %%ax, %%ss \n\
+    movw %%ax, %%ds \n"
+    :
+    : "i"(MAKE_SELECTOR (KERNEL_CODE_SEGMENT, 0, 0)), 
+      "i"(MAKE_SELECTOR (KERNEL_DATA_SEGMENT, 0, 0))
+    :"eax"
+    );
+}
+
 static void load_gdt () {
 
-  gdt_entry_t gdt[5];
+  gdt_entry_t gdt[3];
   gdt[NULL_SEGMENT] = (gdt_entry_t){0,};
-  gdt[KERNEL_CODE_SEGMENT] =  ADD_GDT_ENTRY (0, 0xfffff, 0, 1);
+  gdt[KERNEL_CODE_SEGMENT] = ADD_GDT_ENTRY (0, 0xfffff, 0, 1);
   gdt[KERNEL_DATA_SEGMENT] = ADD_GDT_ENTRY (0, 0xfffff, 0, 0);
-  gdt[USER_CODE_SEGMENT] = ADD_GDT_ENTRY (0, 0xfffff, 3, 1);
+ /* gdt[USER_CODE_SEGMENT] = ADD_GDT_ENTRY (0, 0xfffff, 3, 1);
   gdt[USER_DATA_SEGMENT] = ADD_GDT_ENTRY (0, 0xfffff, 3, 0); 
-
-  gdt_t gdt_desc = (gdt_t){
-    .limit = (sizeof (gdt) - 1),
-    .base = gdt
-  };
+*/
+  gdt_t gdt_desc;
+  gdt_desc.limit = sizeof (gdt) -1;
+  gdt_desc.base = (u32)gdt;
   
   __asm__ volatile (
     "lgdt %0\n"
     :
-    :"m" (gdt_desc)
+    : "m" (gdt_desc)
     : "memory"
     );
 }
 
 extern void switch_to_pm () {
   load_gdt ();
-
+  write_text_vga ("done\n");
+  
+  __asm__ volatile (
+  "movl %%cr0, %%eax\n\
+  orl $1, %%eax\n\
+  movl %%eax, %%cr0\n"
+  :
+  :
+  :"eax"
+  ); 
+  write_text_vga ("done\n");
+  load_segments ();
 }
--- arch/pm.h
+++ arch/pm.h
@@ -2,6 +2,7 @@
 #define _PM_H
 
 #include <include/kernel.h>
+#include <drivers/vga_text.h>
 
 #define NULL_SEGMENT 0
 #define KERNEL_CODE_SEGMENT 1
@@ -31,16 +32,17 @@ typedef struct {
     ((gdt_entry_t){   \
         .limit0_15 = limit & 0xffff,  \
         .base0_15  = base & 0xffff, \
-        .base16_23 = base & 0xff0000 >> 16, \
+        .base16_23 = (base & 0xff0000) >> 16, \
         .type = ((code)? 0xb : 0x3),   \
         .dtype = 1, \
-        .dpl = privilege, \
+        .dpl = (privilege & 0x3), \
         .present = 1, \
-        .limit16_19 = limit & 0xf0000>> 16, \
+        .limit16_19 = (limit & 0xf0000)>> 16, \
         .avl = 0, \
+        .l = 0, \
         .defaut_size = 1, \
         .granularity = 0, \
-        .base24_31 = base & 0xff000000 >> 24 \
+        .base24_31 = (base & 0xff000000) >> 24 \
     })            
                                                 
 
@@ -49,7 +51,18 @@ typedef struct {
   u32 base;
 } __attribute__((packed)) gdt_t;
 
-static void load_gdt ();
+/* ti -> table indicator 0 -> gdt; 1 -> ldt */
+/* rpl -> ring */
+
+#define MAKE_SELECTOR(index, ti, rpl) \
+  ( \
+    (index << 3) | \
+    (ti << 2) | \
+    (rpl) \
+  )
+
 extern void switch_to_pm ();
+
+
 
 #endif /* _PM_H */
