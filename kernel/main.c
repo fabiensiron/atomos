2b58aec3cd6eadff49bf0b721745172796055f11
Saruta <fsiron@orange.fr>
2015-03-24 08:03:53 +0000

    panic done

--- kernel/main.c
+++ kernel/main.c
@@ -1,7 +1,7 @@
 #include <arch/multiboot.h>
 #include <arch/pm.h>
 #include <drivers/vga_text.h>
-
+#include <include/kernel.h>
 
 void kernel_main (unsigned long magic, multiboot_info_t* info) {
   set_bg_color (BLACK); 
@@ -9,11 +9,12 @@ void kernel_main (unsigned long magic, multiboot_info_t* info) {
 
   clear_screen ();
 
-  switch_to_pm ();
-  write_text_vga ("gdt loaded!\n");
+/*  switch_to_pm ();
+  write_text_vga ("gdt loaded!\n"); */
 
-  /*boot_message (); */
+  boot_message (); 
   
+ /* panic (""); */
 
   for (;;)
     continue;
--- kernel/panic.c
+++ kernel/panic.c
@@ -5,6 +5,8 @@ void panic (const char* msg) {
   set_fg_color (RED);
   set_bg_color (BLUE);
 
+  clear_screen ();
+
   write_text_vga ("KERNEL PANIC !!\n\n");
 
   for (;;)
