9673fc7ab8ae32648d3c9150eb03efda00f7ddbe
Saruta <fsiron@orange.fr>
2015-03-24 11:09:09 +0000

    panic improve (new driver version text)

--- kernel/panic.c
+++ kernel/panic.c
@@ -2,10 +2,10 @@
 #include <drivers/vga_text.h>
 
 void panic (const char* msg) {
-  set_fg_color (RED);
-  set_bg_color (BLUE);
 
-  clear_screen ();
+  fill_screen (BLUE);
+
+  set_bg_color (BLUE);
 
   write_text_vga ("KERNEL PANIC !!\n\n");
 
