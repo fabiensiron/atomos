18bc1facc9185a8565554aa6e1e496db8adaa4a3
Saruta <fsiron@orange.fr>
2015-03-26 16:14:57 +0000

    main

--- kernel/main.c
+++ kernel/main.c
@@ -3,18 +3,20 @@
 #include <drivers/vga_text.h>
 #include <include/kernel.h>
 
+
 void kernel_main (unsigned long magic, multiboot_info_t* info) {
   set_bg_color (BLACK); 
   set_fg_color (WHITE);
 
   clear_screen ();
 
-/*  switch_to_pm ();
-  write_text_vga ("gdt loaded!\n"); */
+  switch_to_pm ();
+ // load_gdt();
+  write_text_vga ("gdt loaded!\n"); 
 
   boot_message (); 
-  
- /* panic (""); */
+ /* 
+  panic (""); */
 
   for (;;)
     continue;
