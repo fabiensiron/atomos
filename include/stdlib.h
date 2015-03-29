3d475fc95a96bcb80cbdafabaf7a105689636c6d
Saruta <fsiron@orange.fr>
2015-03-29 19:27:48 +0000

    mem stdlib

--- include/stdlib.h
+++ include/stdlib.h
@@ -3,9 +3,6 @@
 
 #include <include/stddef.h>
 
-int itoa (int n, char* str);
-int atoi (char* str);
-
 void* memset (void*, int, size_t);
 int memcmp (const void*, const void*, size_t);
 void* memcpy (void*, const void*, size_t);
--- lib/stdlib.c
+++ lib/stdlib.c
@@ -1,25 +1,29 @@
 #include <include/stdlib.h>
 
-extern int itoa (int n, char* str) {
-  int i = 0;
-  for (i = 0; str[i] != '\0'; i++) {
-    
-  }
-  return 0;
-}
+extern void* memset (void* p, int c, size_t n) {
+  int* i = NULL;
+  for (i = p; n > 0; n--, i++)
+    *i = c;
 
-extern int atoi (char* str) {
-  return 0;
-}
-
-extern void* memset (void* str, int c, size_t n) {
-  
+  return p;
 }
 
 extern int memcmp (const void* p1, const void* p2, size_t n) {
-  
+  const int* i1 = NULL;
+  const int* i2 = NULL;
+
+  for (i1 = p1, i2 = p2; (*i1 == *i2) && n>0; n--, i1++, i2++)
+    continue;
+
+  return n;
 }
 
-extern void* memcpy (void* str1, const void* str2, size_t n) {
-  
+extern void* memcpy (void* p1, const void* p2, size_t n) {
+  int* i1 = NULL;
+  const int* i2 = NULL;
+
+  for (i1 = p1, i2 = p2; n>0; n--, i1++, i2++)
+    *(i1) = *(i2);
+
+  return p1;
 }
