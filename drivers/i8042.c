9ebfb186a5caedfe16eed23a3b18f75506b8b4a3
Saruta <fsiron@orange.fr>
2015-07-05 18:11:52 +0000

    fix keyboard buffer overflow

--- drivers/i8042.c
+++ drivers/i8042.c
@@ -1,14 +1,22 @@
 #include "i8042.h"
 
-static u8 get_type ();
-static u8 get_scan_code ();
+static u8 get_type (u8 c_) {
+  return (c_ & 0x80)?KEYPRESSED:KEYRELEASED;
+}
+
+static u8 get_scan_code (u8 c_) {
+  return c_ & 0x7f; 
+}
 
 /* just for test */
 
 extern void refresh_ps2_controller () {
+
   while (((INB(I8042_COMMAND)) & 1) == 0)
       ;
+
   char c = INB (0x60);
+
   write_text_vga ("x");
 }
 
@@ -23,10 +31,3 @@ extern void refresh_ps2_controller () {
   }
 } */
 
-static u8 get_type () {
-  return ((INB(I8042_DATA)) & 0x80)?KEYPRESSED:KEYRELEASED;
-}
-
-static u8 get_scan_code () {
-  return (INB (I8042_DATA)) & 0x7f; 
-}
