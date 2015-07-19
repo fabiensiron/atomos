326500975a78a05da163d9d66b614662f2d027bd
Saruta <fsiron@orange.fr>
2015-07-19 21:59:30 +0000

    syscall: add read

--- drivers/i8042.c
+++ drivers/i8042.c
@@ -1,19 +1,18 @@
 #include "i8042.h"
 
-#define SC_NULL 0xff
-
 static char sc_table[] = 
   {
     SC_NULL, SC_NULL,  // 0x0
     '1','2','3','4','5','6','7','8','9','0', // 0x1
     '-','=',SC_NULL,SC_NULL,  // 0x0c
     'q','w','e','r','t','y','u','i','o','p', // 0x10
-    '[',']',SC_NULL,SC_NULL, // 0x1A
+    '[',']',SC_ENTER,SC_NULL, // 0x1A
     'a','s','d','f','g','h','j','k','l', // 0x1e
     ';','\'','`',SC_NULL,'\\', //0x27
     'z','x','c','v','b','n','m', // 0x2c
     ',','.','/',SC_NULL,'*',SC_NULL,' ',SC_NULL, //0x33
-    SC_NULL,SC_NULL,SC_NULL,SC_NULL,SC_NULL,SC_NULL,SC_NULL,SC_NULL, // 0x3B
+    SC_NULL,SC_NULL,SC_NULL,SC_NULL,
+    SC_NULL,SC_NULL,SC_NULL,SC_NULL, // 0x3B
     SC_NULL,SC_NULL, // 0x43
     SC_NULL,SC_NULL, // 0x45
     '7','8','9','-','4','5','6','+', // 0x47
@@ -32,8 +31,18 @@ static u8 get_scan_code (u8 c_) {
 }
 
 static char scan_to_ascii (u8 sc_) {
-  char str[] = {sc_table[sc_], '\0'};
-  write_text_vga (str); 
+  u8 input = sc_table[sc_];
+ // char str[] = {input, '\0'};
+   
+  input_buffer [input_buffer_cursor % 80] = input;
+
+  if (input != SC_NULL) {
+    (input_buffer_cursor)++;
+    if (input != SC_ENTER)
+      kprintf ("%c", input_buffer [(input_buffer_cursor)-1]);
+  }
+
+  return input;
 }
 
 /* just for test */
@@ -49,6 +58,30 @@ extern void refresh_ps2_controller () {
     return;
   u8 sc = get_scan_code (c);
   scan_to_ascii (sc);
+}
+
+extern void wait_input (char* buffer) {
+  for (int i = 0; i< 80; i++)
+    input_buffer[i] = 0;
+
+  input_buffer_cursor = 0;
+
+
+  u8 input = 0x0;
+  while (((input_buffer_cursor) == 0) ||
+        (input_buffer [(input_buffer_cursor) -1] 
+         != SC_ENTER))
+    ;
+      /*||
+    //    (input_buffer[input_buffer_cursor-1] 
+      //  == SC_ENTER)*/ /* wait input */
+    // ;// kprintf ("%c", input_buffer[input_buffer_cursor]);
+    
+    memcpy (buffer, input_buffer, input_buffer_cursor-1);
+    buffer [input_buffer_cursor-1] = '\0';
+
+//    for (int i = 0; buffer[i] != '\0'; i++) 
+  //    kprintf ("%c:%x", buffer[i], buffer[i]);
 }
 
 
--- drivers/i8042.h
+++ drivers/i8042.h
@@ -9,6 +9,8 @@
 #include <include/kernel.h>
 #include <drivers/vga_text.h>
 #include <arch/ioport.h>
+#include <include/stdio.h>
+#include <include/stdlib.h>
 
 #define I8042_DATA 0x60
 #define I8042_COMMAND 0x64
@@ -16,10 +18,16 @@
 #define KEYPRESSED  0
 #define KEYRELEASED 1
 
+#define SC_NULL 0xff
+#define SC_ENTER 0xfe 
+
 bool_t keyboard_new_event;
 u8 keyboard_lastkeypressed;
+u8 input_buffer[80];
+u8 input_buffer_cursor;
 
 extern void refresh_ps2_controller ();
+extern void wait_input (char* buffer);
 
 
 #endif /* _i8042_H */
